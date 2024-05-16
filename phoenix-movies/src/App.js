b26cee192e90817e31130e3c465ca030d5b45c92
Emmanuel Rodrigues da Rocha Costa <rodriguesemanuel021@gmail.com>
2024-05-15 22:58:27 -0300

    commit inicial

---
+++ phoenix-movies/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import React from 'react';
+import './App.css';
+import Main from './pages/Main';
+
+function App() {
+  return (
+    <Main/>
+  );
+}
+
+export default App;
---
+++ phoenix-movies/src/pages/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './main.css' 
+import SideMenu from '../components/SideMenu'
+
+function Main() {
+    return (
+    <main>
+        <SideMenu/>
+        <div className="banner">
+            
+        </div>
+    </main>
+    )
+}
+
+export default Main
