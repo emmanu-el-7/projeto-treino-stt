77ebf73e8d14a991b76c6d9c9ce7b5fc0b4235df
Emmanuel Rodrigues da Rocha Costa <rodriguesemanuel021@gmail.com>
2024-05-17 13:37:37 -0300

    criação do backend + conexão com banco de dados

---
+++ phoenix-movies/backend/knexfile.js
@@ -0,0 +1,13 @@
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+    client: 'pg',
+    connection: {
+        host: 'localhost',
+        database: 'phoenix',
+        user: 'admin',
+        password: '33696549'
+    },
+    debug: true
+}
