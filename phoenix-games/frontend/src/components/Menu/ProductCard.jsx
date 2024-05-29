f6f021b1b5c85a9ba894ac5b2016cc2cc82cb2fa
Emmanuel Rodrigues da Rocha Costa <rodriguesemanuel021@gmail.com>
2024-05-29 15:30:18 -0300

    att menu

--- phoenix-games/frontend/src/components/Menu/ProductCard.jsx
+++ phoenix-games/frontend/src/components/Menu/ProductCard.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import './productCard.css';
-import ProductRating from './ProductRating';
 
 const ProductCard = ({ product }) => {
 	return (
 		<div className='col-xl-3 col-lg-4 col-md-6'>
-			<img src={product.image} alt={product.name} className='img-fluid' />
-			<div className='bag'>
-				<a href='#' className='addBag'>
-					<i className='bi bi-bag-plus-fill'></i>
-				</a>
-				<a href='#'>
-					<i className='bi bi-heart-fill'></i>
-				</a>
-			</div>
-			<div className='productFeature'>
-				<h1 className='product-name'>{product.name}</h1>
-				<ProductRating rating={product.rating} />
-				<span className='product-price'>{product.price}</span>
+			<div className='product-card'>
+				<img src={product.image} alt={product.name} className='img-fluid' />
+				<div className='bag'>
+					<a href='#' className='addBag'>
+						<i className='bi bi-bag-plus-fill'></i>
+					</a>
+					<a href='#'>
+						<i className='bi bi-heart-fill'></i>
+					</a>
+				</div>
+				<div className='productFeature'>
+					<h1 className='product-name'>{product.name}</h1>
+					<h2 className='product-price'>{product.price}</h2>
+				</div>
 			</div>
 		</div>
 	);
