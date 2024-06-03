8d4de53ce9545acd9a6d2e43cefb2b078a95b9b3
Emmanuel Rodrigues da Rocha Costa <rodriguesemanuel021@gmail.com>
2024-06-03 13:41:35 -0300

    instalação material ui

--- phoenix-games/frontend/src/App.js
+++ phoenix-games/frontend/src/App.js
@@ -1,5 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import React from 'react';
 import './App.css';
 import Main from '../src/pages/Home/Main';
