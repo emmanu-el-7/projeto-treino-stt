abfa9b3953f35f3789be5edcc8a3e39ff7a81fc7
Emmanuel Rodrigues da Rocha Costa <rodriguesemanuel021@gmail.com>
2024-06-04 13:52:38 -0300

    att carousel

--- phoenix-games/frontend/src/components/Menu/ProductCarousel.jsx
+++ phoenix-games/frontend/src/components/Menu/ProductCarousel.jsx
@@ -1,10 +1,4 @@
 import React, { useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/navigation';
-import './productcarousel.css';
-import { EffectCoverflow, Navigation, Autoplay } from 'swiper/modules';
 import {
 	Button,
 	IconButton,
@@ -15,6 +9,8 @@ import {
 } from '@mui/material';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import PauseIcon from '@mui/icons-material/Pause';
+import Carousel from 'react-material-ui-carousel';
+import './productcarousel.css';
 
 const ProductCarousel = ({ products }) => {
 	const [activeIndex, setActiveIndex] = useState(null);
@@ -24,67 +20,44 @@ const ProductCarousel = ({ products }) => {
 	};
 
 	return (
-		<Swiper
-			effect={'coverflow'}
-			grabCursor={true}
-			navigation={true}
-			loop={true}
-			centeredSlides={true}
-			slidesPerView={'auto'}
-			spaceBetween={0}
-			coverflowEffect={{
-				rotate: 35,
-				stretch: 0,
-				depth: 250,
-				modifier: 1,
-				slideShadows: true,
-			}}
-			autoplay={{
-				delay: 5000,
-				disableOnInteraction: true,
-			}}
-			modules={[EffectCoverflow, Navigation, Autoplay]}
-			className='productSwiper'
-		>
+		<Carousel>
 			{products.map((product, index) => (
-				<SwiperSlide key={index}>
-					<Card className='productSlider'>
-						<CardMedia
-							component='img'
-							image={product.image}
-							alt='Product Image'
-						/>
-						<div className={`video ${index === activeIndex ? 'active' : ''}`}>
-							<iframe
-								width='1280'
-								height='720'
-								src={product.trailer}
-								title={product.name}
-								allow='accelerometer; clipboard-white; encrypted-media; gyroscope; picture-in-picture'
-								allowFullScreen
-							></iframe>
+				<Card key={index} className='productSlider'>
+					<CardMedia
+						component='img'
+						image={product.image}
+						alt='Product Image'
+					/>
+					<div className={`video ${index === activeIndex ? 'active' : ''}`}>
+						<iframe
+							width='1280'
+							height='720'
+							src={product.trailer}
+							title={product.name}
+							allow='accelerometer; clipboard-white; encrypted-media; gyroscope; picture-in-picture'
+							allowFullScreen
+						></iframe>
+					</div>
+					<CardContent className='content'>
+						<Typography variant='h5' component='h2'>
+							{product.name}
+						</Typography>
+						<div className='buttons'>
+							<Button variant='contained' color='primary' href='#'>
+								Compre já!
+							</Button>
+							<IconButton
+								color='primary'
+								className={`playBtn ${index === activeIndex ? 'active' : ''}`}
+								onClick={() => handleToggleVideo(index)}
+							>
+								{index === activeIndex ? <PauseIcon /> : <PlayArrowIcon />}
+							</IconButton>
 						</div>
-						<CardContent className='content'>
-							<Typography variant='h5' component='h2'>
-								{product.name}
-							</Typography>
-							<div className='buttons'>
-								<Button variant='contained' color='primary' href='#'>
-									Compre já!
-								</Button>
-								<IconButton
-									color='primary'
-									className={`playBtn ${index === activeIndex ? 'active' : ''}`}
-									onClick={() => handleToggleVideo(index)}
-								>
-									{index === activeIndex ? <PauseIcon /> : <PlayArrowIcon />}
-								</IconButton>
-							</div>
-						</CardContent>
-					</Card>
-				</SwiperSlide>
+					</CardContent>
+				</Card>
 			))}
-		</Swiper>
+		</Carousel>
 	);
 };
 
